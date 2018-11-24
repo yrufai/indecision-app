adedf2519c15212a36a1ab12ccba0efdcd7a0b1e
yakubu rufai mahatma <yrufai23@gmail.com>
2018-11-24 11:46:06 +0000

    seperate dependencies from devdependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,44 @@
 const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin")
 
 
-module.exports={
-    entry: "./src/app.js",
-    output: {
-        path: path.join(__dirname, "public" ),
-        filename: "[name]-bundle.js"
-    },
-    module: {
-        rules: [
-            {
-            use: "babel-loader",
-            test: /\.js$/,
-            exclude: /node_modules/
-            },
-            {
-                use: [
-                    "style-loader",
-                    "css-loader",
-                    "sass-loader"
-                ],
-                test: /\.s?css$/,
-            }
-        ]
-    },
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        compress: true,
-        port: 8080
-      },
-      devtool: "cheap-module-eval-source-map"
-    
+module.exports = (env)=>{
+    const isProduction = env === "production"
+    const CSSExtract = new ExtractTextPlugin("styles.css");
+
+    return {
+        entry: "./src/app.js",
+        output: {
+            path: path.join(__dirname, "public" ),
+            filename: "bundle.js"
+        },
+        module: {
+            rules: [
+                {
+                use: "babel-loader",
+                test: /\.js$/,
+                exclude: /node_modules/
+                },
+                {
+                    test: /\.s?css$/,
+                    use: CSSExtract.extract({
+                        use: [
+                            "css-loader",
+                            "sass-loader"
+                        ]
+                    }),
+                }
+            ]
+        },
+        plugins: [
+            CSSExtract
+        ],
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            compress: true,
+            port: 8080
+          },
+          devtool: isProduction ? "source-map" : "cheap-module-eval-source-map"
+        
+    }
 }
